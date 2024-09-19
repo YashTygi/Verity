f4ec9d2966ca70e995302755787cc4ade95af1f9
Yash Tyagi <tygi.yash@gmail.com>
2024-09-19 11:51:42 +0530

    language reduction

--- src/components/LanguageDropDown/LanguageDropDown.jsx
+++ src/components/LanguageDropDown/LanguageDropDown.jsx
@@ -24,12 +24,6 @@ const LanguageDropDown = () => {
                     <option value="हिन्दी">हिन्दी</option>
                     <option value="मराठी">मराठी</option>
                     <option value="தமிழ்">தமிழ்</option>
-                    <option value="తెలుగు">తెలుగు</option>
-                    <option value="ಕನ್ನಡ">ಕನ್ನಡ</option>
-                    <option value="മലയാളം">മലയാളം</option>
-                    <option value="বাংলা">বাংলা</option>
-                    <option value="ਪੰਜਾਬੀ">ਪੰਜਾਬੀ</option>
-                    <option value="ગુજરાતી">ગુજરાતી</option>
                 </select>
             </div>
         </div>
